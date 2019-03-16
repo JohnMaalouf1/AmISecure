6ae2590aa7cdc28c7d07ff5ccdfa6a9c5b697b4d
Michael Sexton <michaeljsexton21@gmail.com>
2019-03-16 15:58:48 -0400

    Added trial password counter

--- md5Hash.py
+++ md5Hash.py
@@ -3,12 +3,16 @@ from time import time
 from itertools import count, product
 from string import printable
 
+counter = 0
 
 def passwords(encodeType):
-    possibleChars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    global counter
     chars = [i.encode(encodeType) for i in printable]
     for length in count(start=1):
         for pwd in product(chars, repeat=length):
+            counter = counter + 1
+            if counter % 100 ==0:
+               print(counter)
             #print(pwd)
             yield b''.join(pwd)
 
@@ -43,6 +47,7 @@ def main():
             hashedPassword = str(input("Please enter your password: "))
             break
 
+    
     start = time()
     cracked = crack(hashedPassword, encodeType)
     end = time()
